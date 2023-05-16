7c46ef00eefc26b61967be8551a2114ba2d22274
nawir177 <m.nawir177@gmail.com>
2023-05-16 23:59:57 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#20232F",
+        primary: "#01EDFF",
+        secondary: "#323744"
+      },
+    },
+  },
+  plugins: [],
+}
+
